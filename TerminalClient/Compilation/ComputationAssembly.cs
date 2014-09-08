21476c503dfb3bab7e6a462eefbd3320b5b1a7e0
HeIIoKitty <andrew.blakely@ymail.com>
2014-09-08 15:17:26 -0500

    Update ComputationAssembly.cs
    Working on dynamic assembly loading for computation.

--- TerminalClient/Compilation/ComputationAssembly.cs
+++ TerminalClient/Compilation/ComputationAssembly.cs
@@ -1,1 +1,36 @@
-//
+using System;
+using System.Codedom;
+
+
+namespace Distributed.Compile
+{
+	///<Summary>
+	/// Provides a wrapper for the dyanmic assembly, method substituion and etc.
+	/// It's important that this class only be created once or we will suffer memory issues
+	/// as dynamic assemblies in the same AppDomain cannot be freed up.
+	///<Summary>
+	public sealed class ComputationAssembly
+	{
+		//Jon Skeet's thread safe singleton design
+		private static readonly ComputationAssembly _instance = new ComputationAssembly();
+		
+		private ComputationAssembly()
+		{
+			
+		}
+		
+		public static ComputationAssembly Instance
+		{
+			get { return _instance; }
+		}
+		
+		#region Dynamic Assembly Members
+		
+		
+		
+		
+		#endregion
+		
+		public RetrieveDynamicAssembly()
+	}
+}
