465b45d112cd44c8d4ca34ba5dbf5f4b86df281b
HeIIoKitty <andrew.blakely@ymail.com>
2014-09-10 13:06:37 -0500

    Added some tiny functionality to the WorkOrder
    The WorkOrder will be the packet type that delivers a request to the server for terminal clients to preform computation.

--- Common/Work Structures/WorkOrder.cs
+++ Common/Work Structures/WorkOrder.cs
@@ -8,12 +8,32 @@ using System.Threading.Tasks;
 namespace Distributed.Work
 {
 	[ProtoContract]
-	public class WorkOrder
+	public class WorkOrder : PacketType
 	{
-		//Protobuf constructor
-		protected WorkOrder()
+		[ProtoMember(1, IsRequired = true)]
+		public ICodePackage ComputationInfoContainer { get; private set; }
+		
+		[ProtoMember(2, IsRequired = true)]
+		public ICodePacakge WorkDelegationContainer { get; private set; }
+		
+		//Protobuf-net constructor
+		protected WorkOrder(ICodePacakge compInfo, ICodePackage workDelegate)
 		{
-
+			ComputationInfoContainer = compInfo;
+			WorkDelegateContainer = workDelegate;
 		}
+		
+		#region Packet Validation Methods
+		public override bool ValidatePacket()
+		{
+			
+		}
+		
+		private bool ValidateICodePackage()
+		{
+			
+		}
+		#endregion
+		
 	}
 }
