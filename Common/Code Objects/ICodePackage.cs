3b77aeead849249d6d7b5024873f6d7eacff9404
HeIIoKitty <andrew.blakely@ymail.com>
2014-09-10 11:13:43 -0500

    Update and rename CodePackage.cs to ICodePackage.cs
    Refactoring this to an interface to be implemented on the a WorkOrder structure.

---
+++ Common/Code Objects/ICodePackage.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CSharp;
+using ProtoBuf;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Distributed.Compile;
+using Distributed.Networking;
+
+namespace Distributed.Code
+{
+	/// <summary>
+	/// Class will be a sub-package that the WorkOrder package packet will be carrying
+	/// when it heads to the master server from the master client to then be handled
+	/// there.
+	/// </summary>
+	[ProtoContract]
+	public interface ICodePackage
+	{
+		/// <summary>
+		/// Members indicates the version number for a given executable
+		/// package of code.
+		/// </summary>
+		[ProtoMember(1, IsRequired = true)]
+		int Version { get; protected set; }
+
+		[ProtoMember(2, IsRequired=true)]
+		CodeData SerializerReadyCodeData { get; protected set; }
+		
+		[ProtoMember(3, IsRequired=true)]
+		ComputationMethod.MSLanguage Language { get; protected set; }
+		
+	}
+}
