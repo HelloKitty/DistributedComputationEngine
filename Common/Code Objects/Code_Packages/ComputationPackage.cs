b1585ab39d458f55df71734a5b871fec2a619774
HeIIoKitty <andrew.blakely@ymail.com>
2014-09-10 13:09:04 -0500

    Added functionality to ComputationPackage
    Implemented the ICodePackage interface and wrote some code that'll handle computation type methods to be added to a collection.
    
    Providing implementation for potentially parsing results and doing something with them on the terminal clients before networking something back to the server to be distributed to the master client.

--- Common/Code Objects/Code_Packages/ComputationPackage.cs
+++ Common/Code Objects/Code_Packages/ComputationPackage.cs
@@ -1,1 +1,64 @@
-//
+
+namespace Distributed.Code
+{
+	[ProtoContract]
+	public class ComputationPackage : ICodePackage
+	{
+		public enum ComputationMethodType : byte
+		{
+			MainComputation,
+			ResultsHandler
+		}
+		
+		public int Version { get; protected set; }
+		
+		public ComputationMethod.MSLanguage Language { get; protected set; }
+		
+		[ProtoMember(3, IsRequired = true)]
+		private IDictionary<byte, CodeData> SerializerReadyCodeCollection { get; private set; }
+		
+		public ComputationPackage(ComputationMethod info)
+		{
+			Version = info.Version;
+			Language = info.Language;
+			SerializerReadyCodeCollection = Dictionary<byte, CodeData>(3);
+		}
+		
+		#region ICodePackage Explict Method Implementation
+		bool ICodePackage.RegisterMethod(CodeData codeData, byte typeByteSignifier)
+		{
+			if(SerializerReadyCodeCollection.HasKey(typeByteSignifier) 
+			&& SerializerReadyCodeCollection[typeByteSignifier] != null)
+				return false;
+				
+			SerializerReadyCodeCollection[typeByteSignifier] = codeData
+		}
+		
+		bool ICodePackage.TryGetMethod(byte typeByteSignifier, out CodeData data)
+		{
+			if(!SerializerReadyCodeCollection.HasKey(typeByteSignifier) 
+			|| SerializerReadyCodeCollection[typeByteSignifier] == null)
+			{
+				data = null;
+				return false;
+			}
+			
+			data = SerializerReadyCodeCollection[typeByteSignifier];
+			return true;
+		}
+		#endregion
+		
+		#region Methods that 'hide' ICodePackage methods
+		public bool RegisterMethod(CodeData codeData, ComputationMethodType type)
+		{
+			return ICodePackage.RegisterMethod((byte)type);
+		}
+		
+		public bool TryGetMethod(byte typeByteSignifier, out CodeData data)
+		{
+			return ICodePackage.RegisterMethod((byte)type);
+		}
+		#endregion
+		
+	}
+}
