0402d711c644cb79a65ea0fe39099b2c7bd6eea6
HeIIoKitty <andrew.blakely@ymail.com>
2014-09-12 09:30:01 -0500

    Update AParser.cs
    Preparing parsing abstract class.

--- Common/Code Objects/Parsing/AParser.cs
+++ Common/Code Objects/Parsing/AParser.cs
@@ -1,1 +1,18 @@
 //
+
+namespace Distributed.Parsing
+{
+	public abstract class AParser : IDisposable
+	{
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		
+		protected virtual void Dispose(bool disposing)
+		{
+			//Implement in inherited classes that need their resources freed up.
+		}
+	}
+}
